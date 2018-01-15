993bf3ba39f45e13e461016cdd20d46a1db9f39c
Jesús Fco Cortés <jfcr@live.com>
2018-01-15 07:23:32 -0600

    Sale save work()

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Sale;
 use App\Description;
+use App\Shop;
 use Illuminate\Support\Facades\DB;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
@@ -13,6 +14,8 @@ class SaleController extends Controller
     public function storeSale(Request $request){
         $user = JWTAuth::parseToken()->authenticate();
 
+        // return response()->json('Sale Store');
+
         $sale = new sale();
         $sale->setTable('sales'. $user->shop_id);
         $sale->total = $request->total;
@@ -20,17 +23,30 @@ class SaleController extends Controller
         $sale->created_at = date_create();
         $sale->save();
 
-        foreach($x as $request->description){
+        foreach($request->description as $x){
             $description = new Description();
             $description->setTable('sale_description' . $user->shop_id);
             $description->sale_id = $sale->id;
-            $description->product_id = $x['id_product'];
             $description->product_id = $x['product_id'];
-            $description->product_id = $x['product_id'];
+            $description->price = $x['price'];
+            $description->quantity = $x['quantity'];
+            $description->subtotal = $x['subtotal'];
+            $description->save();
+
+            DB::table('products'. $user->shop_id)
+                ->where('id', $x['product_id'])
+                ->decrement('stock', $x['quantity']);
         }
+        
+        if($user->cash == NULL){
+            $shop = Shop::find($user->shop_id);
+            $shop->cash += $sale->total;
+            $shop->save();
+        }
+        return response()->json('Sale Store');
     }
 
-    public function storeSale(Request $request){
+    public function storeSaleOutService(Request $request){
         
     }
 }
